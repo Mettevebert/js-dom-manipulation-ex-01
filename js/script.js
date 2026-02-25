51bfec5e821ad8dd55a83016f48a56736f31993b
Mettevebert <mettevebert@gmail.com>
2026-02-25 14:31:00 +0100

    first commit
    set up script - as per task no. one

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const websiteBody = document.body;
+
+const headingOne = document.querySelector("h1");
+
+websiteBody.style.background = "lightblue";
+
+headingOne.style.fontSize = "3rem";
+headingOne.style.color = "Maroon";
+
